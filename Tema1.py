ddd75d8f606de2c5e7ab944035c737419e3f37c5
Iulian-Coroban <iulian.coroban62@gmail.com>
2026-03-08 19:59:11 +0200

    Am adaugat si lambda-NFA

--- Tema1.py
+++ Tema1.py
@@ -80,16 +80,73 @@ class NFA:
 
     def tranzitii_utilizate(self):
         return self.tranzitii_utilizate_la_ultima_verificare
+    
+
+
+
+class LAMBDA_NFA:
+    alfabet = set()
+    stari = []
+    stari_finale = []
+    legaturi = {}
+    tranzitii_utilizate_la_ultima_verificare = []
+    def __init__(self,alfabet, stari, stari_finale, legaturi):
+        self.alfabet = set(alfabet)
+        self.stari = stari
+        self.stari_finale = stari_finale
+        legaturidict = {}
+        for legatura in legaturi:
+            stare_initiala, stare_finala, litera = legatura.strip().split()
+            if legaturidict.get(stare_initiala) is None:
+                legaturidict[stare_initiala] = {litera : [stare_finala]}
+            else:
+                if legaturidict[stare_initiala].get(litera) is None:
+                    legaturidict[stare_initiala][litera] = [stare_finala]
+                else:
+                    legaturidict[stare_initiala][litera].append(stare_finala)
+        self.legaturi = legaturidict
+
+    def __str__(self):
+        return ", ".join(self.alfabet)
+    
+    def verifica_cuvant(self, cuvant):
+        def recursie(stare_curenta, cuvant_curent):
+            if len(cuvant_curent) == 0:
+                return stare_curenta in self.stari_finale
+            litera = cuvant_curent[0]
+            legaturi_stare_curenta = self.legaturi.get(stare_curenta, {})
+            stari_urmatoare = legaturi_stare_curenta.get(litera, [])
+            stari_urmatoare_lambda = legaturi_stare_curenta.get("lambda", [])
+            for stare_urmatoare in stari_urmatoare:
+                self.tranzitii_utilizate_la_ultima_verificare.append((stare_curenta, litera))
+                if recursie(stare_urmatoare, cuvant_curent[1:]):
+                    return True
+                self.tranzitii_utilizate_la_ultima_verificare.pop()
+            for stare_urmatoare_lambda in stari_urmatoare_lambda:
+                self.tranzitii_utilizate_la_ultima_verificare.append((stare_curenta, "lambda"))
+                if recursie(stare_urmatoare_lambda, cuvant_curent):
+                    return True
+                self.tranzitii_utilizate_la_ultima_verificare.pop()
+            return False
+        return recursie(self.stari[0], cuvant)            
+
+    def tranzitii_utilizate(self):
+        return self.tranzitii_utilizate_la_ultima_verificare
 
     
 #Verificari
 
-d1 = DFA(("a", "b"), ["q0"], ["q1"], ["q0 q1 a", "q1 q0 b"])
-print("DA" if d1.verifica_cuvant("a") else "Nu")
-print(d1.tranzitii_utilizate())
-print(d1)
+# d1 = DFA(("a", "b"), ["q0"], ["q1"], ["q0 q1 a", "q1 q0 b"])
+# print("DA" if d1.verifica_cuvant("a") else "Nu")
+# print(d1.tranzitii_utilizate())
+# print(d1)
 
-n1 = NFA(("a", "b"), ["q0"], ["q1"], ["q0 q1 a", "q0 q1 b"])
-print("Da" if n1.verifica_cuvant("a") else "Nu")
-print(n1.tranzitii_utilizate())
-print(n1)
+# n1 = NFA(("a", "b"), ["q0"], ["q1"], ["q0 q1 a", "q0 q1 b"])
+# print("Da" if n1.verifica_cuvant("a") else "Nu")
+# print(n1.tranzitii_utilizate())
+# print(n1)
+
+l1 = LAMBDA_NFA(("a", "b"), ["q0","q1","q2"], ["q2"], ["q0 q1 lambda", "q1 q1 a", "q1 q2 b"])
+print("Da" if l1.verifica_cuvant("aaa") else "Nu")
+# print(l1.tranzitii_utilizate())
+# print(l1)
